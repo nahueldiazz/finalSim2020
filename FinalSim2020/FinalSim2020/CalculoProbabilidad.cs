e79b1d39015a3490dea12b47bb132a0e0a2737d7
@diaz.nahuel <nahuel.diazz.nd@gmail.com>
2020-07-19 17:14:55 -0300

    avances

---
+++ FinalSim2020/FinalSim2020/CalculoProbabilidad.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalSim2020
+{
+    public class CalculoProbabilidad
+    {
+        double normal = 0;
+        double desviacionEstandar = 0;
+        double nroACombustible = 0;
+        double nroBCombustible = 0;
+        double nroAGomeria = 0;
+        double nroBGomeria = 0;
+        double nroAAccesorios = 0;
+        double nroBAccesorios = 0;
+        double sumaRnd = 0;
+
+        string combustible = "Carga Combustible";
+        string gomeria = "Gomería";
+        string accesorios = "Accesorios";
+        string seRetira = "Se retira";
+
+
+        Random rnd = new Random();
+
+        public CalculoProbabilidad(double normalCargada, double ds, double Acomb, double Bcomb, double Agom, double Bgom, double Aacce, double Bacc)
+        {
+            normal = normalCargada;
+            desviacionEstandar = ds;
+            nroACombustible = Acomb;
+            nroBCombustible = Bcomb;
+            nroAGomeria = Agom;
+            nroBGomeria = Bgom;
+            nroAAccesorios = Aacce;
+            nroBAccesorios = Bacc;
+        }
+
+        public double TruncateFunction(double number, int digit)
+        {
+            return Math.Truncate((Math.Pow(10.0, (double)digit) * number)) / (Math.Pow(10.0, (double)digit));
+        }
+
+        public double LlegadaCliente()
+        {
+            sumaRnd = 0;
+
+            for (int i = 0; i < 12; i++)
+            {
+                sumaRnd += this.TruncateFunction(rnd.NextDouble(), 4);
+            }
+            sumaRnd = this.TruncateFunction(sumaRnd, 4);
+
+            return ((sumaRnd - 6) * desviacionEstandar) + normal;
+        }
+
+        public double AtencionCombustible()
+        {
+            var nuevoRnd = this.TruncateFunction(rnd.NextDouble(), 4);
+
+            return nroACombustible + (nuevoRnd * (nroBCombustible - nroACombustible));
+        }
+
+        public double AtencionGomeria()
+        {
+            var nuevoRnd = this.TruncateFunction(rnd.NextDouble(), 4);
+
+            return nroAGomeria + (nuevoRnd * (nroBGomeria - nroAGomeria));
+        }
+
+        public double AtencionAccesorios()
+        {
+            var nuevoRnd = this.TruncateFunction(rnd.NextDouble(), 4);
+
+            return nroAAccesorios + (nuevoRnd * (nroBAccesorios - nroAAccesorios));
+        }
+
+
+        public string TipoServicio()
+        {
+            var nuevoRnd = this.TruncateFunction(rnd.NextDouble(), 2);
+            if (nuevoRnd < 0.80)
+            {
+                return combustible;
+            }
+            else
+            {
+                var otroRnd = this.TruncateFunction(rnd.NextDouble(), 2);
+                if (otroRnd < 0.40) return accesorios;
+                return gomeria;
+            }
+        }
+
+        public string PosCargaCombustible()
+        {
+            var nuevoRnd = this.TruncateFunction(rnd.NextDouble(), 2);
+
+            if (nuevoRnd < 0.30) return accesorios;
+            if (nuevoRnd < 0.50) return gomeria;
+            return seRetira;
+        }
+
+
+
+    }
+}
--- FinalSim2020/FinalSim2020/Form1.cs
+++ FinalSim2020/FinalSim2020/Form1.cs
@@ -12,9 +12,158 @@ namespace FinalSim2020
 {
     public partial class Form1 : Form
     {
+        //eventos 
+        string llegadaCliente = "Llegada_cliente";
+        string fin_atencion_combustible = "Fin_atencion_combustible";
+        string fin_atencion_accesorios = "Fin_atencion_accesorios";
+        string fin_atenicon_gomeria = "Fin_atencion_gomeria";
+        string inicializacion = "Inicializacion";
+        string finalizacion = "Fin de simulacion";
+
+        //declaracion de variable para calculo de probabilidades 
+        CalculoProbabilidad proxNum;
+
+        //carga de datos
+        double minutosASimular = 0;
+        double desdeSim = 0;
+        double hastaSim = 0;
+        double normalIn = 0;
+        double dsIn = 0;
+        double nAcomb = 0;
+        double nBcomb = 0;
+        double nAgom = 0;
+        double nBgom = 0;
+        double nAacc = 0;
+        double nBacc = 0;
+
+        //datos grilla 
+        string evento = "";
+        double reloj = 0;
+        double proxLlegadaCliente = 0;
+        double llegaClienteNum = 0;
+        double tipoServicio = 0;
+        double tiempoAtencionCom = 0;
+        double finAtencionCom1 = 0;
+        double finAtencionCom2 = 0;
+        double finAtencionCom3 = 0;
+        double colaCombustible = 0;
+        string tipoServicioPostCombustible = "";
+        double tiempoAtencionGomeria = 0;
+        double finAtencionGom1 = 0;
+        double finAtencionGom2 = 0;
+        double colaGomeria = 0;
+        double tiempoAtencionAccesorios = 0;
+        double finAtencionAcessorios = 0;
+        double colaAceesorios = 0;
+
+
+        List<Cliente> colaCombustibleconCliente = new List<Cliente>();
+        List<Cliente> colaGomeriaconCliente = new List<Cliente>();
+        List<Cliente> colaAccesoriosconCliente = new List<Cliente>();
+        List<Cliente> grillaconClientesfinalizados = new List<Cliente>();
+
+
+        Cliente cliente;
+
+
+
+
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Simular_Click(object sender, EventArgs e)
+        {
+            this.inicializarValoresIngresados();
+            this.iniciarSimulacion();
+
+
+        }
+
+        public void inicializarValoresIngresados()
+        {
+            normalIn = double.Parse(media_normal.Text);
+            dsIn = double.Parse(desv_normal.Text);
+            nAcomb = double.Parse(nroA_comb.Text);
+            nBcomb = double.Parse(nroB_comb.Text);
+            nAgom = double.Parse(nroA_gom.Text);
+            nBgom = double.Parse(nroB_gom.Text);
+            nAacc = double.Parse(nroA_acc.Text);
+            nBacc = double.Parse(nroB_acc.Text);
+
+            proxNum = new CalculoProbabilidad(normalIn, dsIn, nAcomb, nBcomb, nAgom, nBgom, nAacc, nBacc);
+
+            minutosASimular = double.Parse(minutos_a_simular.Text);
+            desdeSim = double.Parse(desde_min_sim.Text);
+            hastaSim = double.Parse(hasta_min_sim.Text);
+
+
+        }
+
+        public void iniciarSimulacion()
+        {
+            proxLlegadaCliente = proxNum.LlegadaCliente();
+            llegaClienteNum = proxLlegadaCliente;
+
+            tabla_estacion.Rows.Add(inicializacion, reloj, proxLlegadaCliente, llegaClienteNum, tipoServicio, tiempoAtencionCom, finAtencionCom1, finAtencionCom2, finAtencionCom3, colaCombustible, tipoServicioPostCombustible, tiempoAtencionGomeria, finAtencionGom1, finAtencionGom2, colaGomeria, tiempoAtencionAccesorios, finAtencionAcessorios, colaAceesorios);
+
+            while (reloj <= minutosASimular)
+            {
+                this.proximoEvento();
+                var valorSwitch = this.nombreEvento();
+                switch (valorSwitch)
+                {
+                    //llegada cliente
+                    case 1: break;
+                    //fin atencion combustible
+                    case 2: break;
+                    //fin atencion gomeria
+                    case 3: break;
+                    //fin atencion accesorios
+                    case 4: break;
+                }
+                if (reloj >= desdeSim && reloj <= hastaSim)
+                {
+                    tabla_estacion.Rows.Add(inicializacion, reloj, proxLlegadaCliente, llegaClienteNum, tipoServicio, tiempoAtencionCom, finAtencionCom1, finAtencionCom2, finAtencionCom3, colaCombustible, tipoServicioPostCombustible, tiempoAtencionGomeria, finAtencionGom1, finAtencionGom2, colaGomeria, tiempoAtencionAccesorios, finAtencionAcessorios, colaAceesorios);
+
+                }
+
+            }
+
+            tabla_estacion.Rows.Add(inicializacion, reloj, proxLlegadaCliente, llegaClienteNum, tipoServicio, tiempoAtencionCom, finAtencionCom1, finAtencionCom2, finAtencionCom3, colaCombustible, tipoServicioPostCombustible, tiempoAtencionGomeria, finAtencionGom1, finAtencionGom2, colaGomeria, tiempoAtencionAccesorios, finAtencionAcessorios, colaAceesorios);
+
+        }
+
+        public void proximoEvento()
+        {
+            List<double> num = new List<double>();
+
+            if (llegaClienteNum != 0) num.Add(llegaClienteNum);
+            if (finAtencionCom1 != 0) num.Add(finAtencionCom1);
+            if (finAtencionCom2 != 0) num.Add(finAtencionCom2);
+            if (finAtencionCom3 != 0) num.Add(finAtencionCom3);
+            if (finAtencionGom1 != 0) num.Add(finAtencionGom1);
+            if (finAtencionGom2 != 0) num.Add(finAtencionGom2);
+            if (finAtencionAcessorios != 0) num.Add(finAtencionAcessorios);
+            reloj = num.Min();
+
+        }
+
+        public int nombreEvento()
+        {
+            if (reloj == llegaClienteNum) return 1;
+            if (reloj == finAtencionCom1) return 2;
+            if (reloj == finAtencionCom2) return 2;
+            if (reloj == finAtencionCom3) return 2;
+            if (reloj == finAtencionGom1) return 3;
+            if (reloj == finAtencionGom2) return 3;
+            return 4;
+
         }
     }
 }
